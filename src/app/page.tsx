d0bdabbcc49afd796fb4da738741a35c76c65673
Shiva Krishna <shivakrishna6032@gmail.com>
2025-07-13 19:17:43 +0530

    feat: globals.css setup

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="text-3xl flex align-center justify-center h-screen bg-primary text-primary-foreground font-bold underline">
+      Welcome
+    </div>
+  );
+} 
