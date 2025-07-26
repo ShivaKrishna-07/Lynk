d34544d003aa2b0aa38b02eb6e75530a0ab3a67d
Shiva Krishna <shivakrishna6032@gmail.com>
2025-07-27 00:51:12 +0530

    feat: add footer section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Features from "@/components/LandingComponents/Features";
 import About from "@/components/LandingComponents/About";
 import { WhoIsItFor } from "@/components/LandingComponents/WhoIsItForSection";
 import PricingSection from "@/components/LandingComponents/PricingSection";
+import LandingFooter from "@/components/LandingComponents/LandingFooter";
 
 export default function Page() {
   return (
@@ -15,6 +16,7 @@ export default function Page() {
       <About/>
       <WhoIsItFor/>
       <PricingSection/>
+      <LandingFooter/>
     </div>
   );
 }
